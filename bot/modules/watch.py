d32868d62e48771778602e4d3d2984cfeabc25d1
kang-newbie <adlizhafari122@gmail.com>
2022-02-01 00:32:18 +0700

    shit typo

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -69,7 +69,7 @@ def _watch(bot, update, isZip=False, isLeech=False, pswd=None, tag=None):
     try:
         result = ydl.extractMetaData(link, name, True)
     except Exception as e:
-        msg = str(e).replace('<', ' ').replace('>', ' ').repalce(';','').split('please report this issue on')[0]
+        msg = str(e).replace('<', ' ').replace('>', ' ').replace(';','').split('please report this issue on')[0]
         return sendMessage(f"⚠️ {tag} {msg.strip()}", bot, update)
     if 'entries' in result:
         for i in ['144', '240', '360', '480', '720', '1080', '1440', '2160']:
