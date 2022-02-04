56375b3afd760b439a8e38aef3d434882c3c33ae
kang-newbie <adlizhafari122@gmail.com>
2022-02-04 16:19:46 +0700

    revert aria.sh

--- aria.sh
+++ aria.sh
@@ -4,7 +4,7 @@ aria2c --enable-rpc=true --check-certificate=false --daemon=false \
    --bt-stop-timeout=300 --min-split-size=10M --split=10 --allow-overwrite=true \
    --max-overall-download-limit=0 --bt-tracker="[$tracker_list]" --disk-cache=32M \
    --max-overall-upload-limit=1K --max-concurrent-downloads=15 --summary-interval=0 \
-   --peer-id-prefix=-qB4400- --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0" \
-   --peer-agent=qBittorrent/4.4.0 --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 \
-   --max-tries=20 --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true --continue=true \
+   --peer-id-prefix=-qB4400- --user-agent=Wget/1.12 --peer-agent=qBittorrent/4.4.0 \
+   --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 --max-tries=20 \
+   --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true --continue=true \
    --content-disposition-default-utf8=true --netrc-path=/usr/src/app/.netrc
