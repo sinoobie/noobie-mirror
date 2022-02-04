04fba3f463cde1dfb62001b3a0a61e0479e9d1e2
kang-newbie <adlizhafari122@gmail.com>
2022-02-04 14:19:31 +0700

    fix sourceforge.net

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -71,8 +71,8 @@ def direct_link_generator(link: str, host):
         return solidfiles(link)
     elif 'krakenfiles.com' in host:
         return krakenfiles(link)
-    elif 'sourceforge.net' in host:
-        return link.rstrip("/download")
+#    elif 'sourceforge.net' in host:
+#        return link.rstrip("/download")
     elif is_gdtot_link(link):
         return gdtot(link)
     elif any(x in host for x in fmed_list):
