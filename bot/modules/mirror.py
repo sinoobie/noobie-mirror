59d529147419c4d48186be85ed28fb90a488a85d
nekoru-kun <93059693+nekoru-kun@users.noreply.github.com>
2022-04-03 12:12:37 +0700

    increase multi links delay

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -470,7 +470,7 @@ def _mirror(bot, message, isZip=False, extract=False, isQbit=False, isLeech=Fals
         Thread(target=add_aria2c_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}', listener, name)).start()
 
     if multi > 1:
-        sleep(6)
+        sleep(10)
         nextmsg = type('nextmsg', (object, ), {'chat_id': message.chat_id, 'message_id': message.reply_to_message.message_id + 1})
         nextmsg = sendMessage(message_args[0], bot, nextmsg)
         multi -= 1
