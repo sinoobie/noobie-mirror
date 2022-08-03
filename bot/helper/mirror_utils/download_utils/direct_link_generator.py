a47ffe7a66ac58056f22b5e811fab95e7fa6d25c
nekoru-kun <93059693+nekoru-kun@users.noreply.github.com>
2022-08-04 03:22:39 +0700

    also this one

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -627,7 +627,7 @@ def appdrive(url: str) -> str:
     elif 'error' in response and response['error']:
         info_parsed['error'] = True
         info_parsed['error_message'] = response['message']
-    if urlparse(url).netloc in appdrive_family and not info_parsed['error']:
+    if any(x in url for x in appdrive_family) and not info_parsed['error']:
         res = client.get(info_parsed['gdrive_link'])
         drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn')]/@href")[0]
         info_parsed['gdrive_link'] = drive_link
