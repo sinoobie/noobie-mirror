68162e7a64f240a3b22d68f2ef886a28af24e8f9
kang-newbie <adlizhafari122@gmail.com>
2022-09-24 22:32:52 +0700

    revert

--- bot/helper/mirror_utils/download_utils/mega_downloader.py
+++ bot/helper/mirror_utils/download_utils/mega_downloader.py
@@ -125,18 +125,18 @@ class MegaDownloader:
             arch = any([self.__listener.isZip, self.__listener.extract])
             limit = None
             if self.__listener.isLeech and LEECH_LIMIT:
-                mssg = f'Leech limit {LEECH_LIMIT}GB'
+                msg3 = f'Leech limit {LEECH_LIMIT}GB'
                 limit = LEECH_LIMIT
             elif arch and ZIP_UNZIP_LIMIT:
-                mssg = f'Zip/Unzip limit {MEGA_LIMIT}GB'
-                limit = MEGA_LIMIT
+                msg3 = f'Zip/Unzip limit {ZIP_UNZIP_LIMIT}GB'
+                limit = ZIP_UNZIP_LIMIT
             elif MEGA_LIMIT is not None:
-                mssg = f'Mega limit {MEGA_LIMIT}GB'
+                msg3 = f'Mega limit {MEGA_LIMIT}GB'
                 limit = MEGA_LIMIT
             if limit is not None:
                 LOGGER.info('Checking File/Folder Size...')
                 if file_size > limit * 1024**3:
-                    self.__listener.onDownloadError(f'{mssg}.\nUkuran file/folder kamu adalah {get_readable_file_size(size)}')
+                    self.__listener.onDownloadError(f'{msg3}.\nUkuran file/folder kamu adalah {get_readable_file_size(size)}')
                     return
         self.__onDownloadStart(file_name, file_size, gid)
         LOGGER.info(f'Mega download started with gid: {gid}')
