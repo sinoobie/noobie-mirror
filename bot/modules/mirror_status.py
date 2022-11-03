08a0fdc028e1e6ca1efcfba7bab17cddecb5c896
kang-newbie <adlizhafari122@gmail.com>
2022-11-04 02:51:06 +0700

    typo

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -38,7 +38,7 @@ def mirror_status(update, context):
 def status_pages(update, context):
     query = update.callback_query
     user_id = query.from_user.id
-    chat_id = update.effective_chat.
+    chat_id = update.effective_chat.id
     auth = context.bot.get_chat_member(chat_id, user_id).status in ['creator', 'administrator'] or user_id in [OWNER_ID]
     data = query.data
     data = data.split()
